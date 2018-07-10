a31cf57dc10d933b2a5ca0c143e38e018e21c771
Marco Siebecke <marco@spotify.com>
2018-07-10 11:46:38 +0200

    Cleaned up project folder and eadded Register8 tests

--- tests/CoreTest.cs
+++ tests/CoreTest.cs
@@ -4,13 +4,38 @@ using NUnit.Framework;
 
 using Core;
 
-namespace Tests
+namespace CoreTests
 {
-    public class project
+    public class RegisterTests
     {
         [SetUp]
         public void Setup()
         {
+        }
+
+        [Test]
+        public void TestRegister8()
+        {
+            byte value = 0;
+            Register8 register = new Register8();
+
+            value = 0;
+            register.setValue(value);
+            Assert.AreEqual(value, register.getValue());
+
+            // all bits set
+            value = 255;
+            register.setValue(value);
+            Assert.AreEqual(value, register.getValue());
+
+            // do some random tests of the register
+            Random rnd = new Random();
+            for(int i = 100; i>0; i--)
+            {
+                value = (byte)rnd.Next(0,255);
+                register.setValue(value);
+                Assert.AreEqual(value, register.getValue());
+            }
         }
 
         [Test]
@@ -24,15 +49,16 @@ namespace Tests
             Assert.AreEqual(value, PC.getValue());
 
             // all bits in PCL
-            value = 256;
+            value = 255;
             PC.setValue(value);
             Assert.AreEqual(value, PC.getValue());
 
             // overflow into PCH
-            value = 257;
+            value = 256;
             PC.setValue(value);
             Assert.AreEqual(value, PC.getValue());
 
+            // do some random tests of the register
             Random rnd = new Random();
             for(int i = 100; i>0; i--)
             {
