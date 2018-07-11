ada339e52c5f12a83a47563667c0491e799cdb2d
Marco Siebecke <marco@spotify.com>
2018-07-11 23:33:05 +0200

    Some more random tests. AND is now also tested

--- tests/CoreTest.cs
+++ tests/CoreTest.cs
@@ -95,7 +95,9 @@ namespace CoreTests
                 Assert.AreEqual(value, cpu.PC);
             }
         }
-
+    }
+    class testCommands
+    {
         [Test]
         public void testORA()
         {
@@ -103,26 +105,59 @@ namespace CoreTests
             ushort addr = 0x002a;
             byte[] blankMemory = new byte[65536];
             CPU6510 cpu = new CPU6510(blankMemory);
+            Random rnd = new Random();
 
-            blankMemory[addr] = 10;
-            cpu.A = 0x01;
-            oldA = cpu.A;
-            cpu.ORA(addr);
-            Assert.AreEqual((oldA|blankMemory[addr]), cpu.A);
-            Assert.AreEqual(0x00, cpu.P);
+            for (int i = 0; i < 100; i++)
+            {
+                blankMemory[addr] = (byte)rnd.Next(0,255);
+                cpu.A = (byte)rnd.Next(0,255);
+                oldA = cpu.A;
+                cpu.ORA(addr);
+                Assert.AreEqual((oldA|blankMemory[addr]), cpu.A);
+            }
 
             blankMemory[addr] = 0x00;
             cpu.A = 0x00;
             oldA = cpu.A;
             cpu.ORA(addr);
-            Assert.AreEqual((oldA|blankMemory[addr]), cpu.A);
             Assert.AreEqual(0x02, cpu.P);
 
             blankMemory[addr] = 0x81;
             cpu.A = 0x02;
             oldA = cpu.A;
             cpu.ORA(addr);
-            Assert.AreEqual((oldA|blankMemory[addr]), cpu.A);
+            Assert.AreEqual(0x80, cpu.P);
+        }
+
+        [Test]
+        public void testAND()
+        {
+            byte oldA;
+            ushort addr = 0x002a;
+            byte[] blankMemory = new byte[65536];
+            CPU6510 cpu = new CPU6510(blankMemory);
+            Random rnd = new Random();
+
+            for (int i = 0; i < 100; i++)
+            {
+                blankMemory[addr] = (byte)rnd.Next(0,255);
+                cpu.A = (byte)rnd.Next(0,255);
+                oldA = cpu.A;
+                cpu.AND(addr);
+                Assert.AreEqual((oldA&blankMemory[addr]), cpu.A);
+            }
+
+            // Test Status Register
+            blankMemory[addr] = 0x05;
+            cpu.A = 0x62;
+            oldA = cpu.A;
+            cpu.AND(addr);
+            Assert.AreEqual(0x02, cpu.P);
+
+            blankMemory[addr] = 0x81;
+            cpu.A = 0x82;
+            oldA = cpu.A;
+            cpu.AND(addr);
             Assert.AreEqual(0x80, cpu.P);
         }
     }
