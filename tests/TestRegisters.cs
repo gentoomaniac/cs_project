cda14fced7d424a85151bccd0a50add8acf1831c
Marco Siebecke <marco@spotify.com>
2018-11-07 11:09:55 +0100

    added missing Asserts and switched to hex notation

--- tests/TestRegisters.cs
+++ tests/TestRegisters.cs
@@ -9,87 +9,95 @@ namespace TestRegisters
 {
     public class RegisterTests
     {
+        private int NUMBER_TEST_RUNS = 10000;
+
         [Test]
         public void TestRegisterPCL()
         {
-            byte value = 0;
+            byte value, pch;
             byte[] blankMemory = new byte[65536];
             Lock cpuLock = new AlwaysOpenLock();
             CPU6510 cpu = new CPU6510(blankMemory, cpuLock);
 
-            value = 0;
+            value = 0x00;
             cpu.PCL = value;
             Assert.AreEqual(value, cpu.PCL);
 
             // all bits set
-            value = 255;
+            value = 0xff;
             cpu.PCL = value;
             Assert.AreEqual(value, cpu.PCL);
 
             // do some random tests of the register
             Random rnd = new Random();
-            for(int i = 100; i>0; i--)
+            for(int i = NUMBER_TEST_RUNS; i>0; i--)
             {
-                value = (byte)rnd.Next(0,255);
+                cpu.PC = (ushort)rnd.Next(0x0000, 0xffff);
+                pch = cpu.PCH;
+                value = (byte)rnd.Next(0x00, 0xff);
                 cpu.PCL = value;
                 Assert.AreEqual(value, cpu.PCL);
+                Assert.AreEqual(pch, cpu.PCH);
             }
         }
 
         [Test]
         public void TestRegisterPCH()
         {
-            byte value = 0;
+            byte value, pcl;
             byte[] blankMemory = new byte[65536];
             Lock cpuLock = new AlwaysOpenLock();
             CPU6510 cpu = new CPU6510(blankMemory, cpuLock);
 
-            value = 0;
+            value = 0x00;
             cpu.PCH = value;
             Assert.AreEqual(value, cpu.PCH);
 
             // all bits set
-            value = 255;
+            value = 0xff;
             cpu.PCH = value;
             Assert.AreEqual(value, cpu.PCH);
 
             // do some random tests of the register
             Random rnd = new Random();
-            for(int i = 100; i>0; i--)
+            for(int i = NUMBER_TEST_RUNS; i>0; i--)
             {
-                value = (byte)rnd.Next(0,255);
+                cpu.PC = (ushort)rnd.Next(0x0000, 0xffff);
+                pcl = cpu.PCL;
+                value = (byte)rnd.Next(0x00, 0xff);
                 cpu.PCH = value;
                 Assert.AreEqual(value, cpu.PCH);
+                Assert.AreEqual(pcl, cpu.PCL);
             }
         }
 
         [Test]
         public void TestRegisterPC()
         {
-            ushort value = 0;
+            ushort value;
             byte[] blankMemory = new byte[65536];
             Lock cpuLock = new AlwaysOpenLock();
             CPU6510 cpu = new CPU6510(blankMemory, cpuLock);
 
-            value = 0;
+            value = 0x00;
             cpu.PC = value;
             Assert.AreEqual(value, cpu.PC);
 
             // all bits in PCL
-            value = 255;
+            value = 0xff;
             cpu.PC = value;
             Assert.AreEqual(value, cpu.PC);
 
             // overflow into PCH
-            value = 256;
+            value = 0x0100;
             cpu.PC = value;
             Assert.AreEqual(value, cpu.PC);
 
             // do some random tests of the register
             Random rnd = new Random();
-            for(int i = 100; i>0; i--)
+            for(int i = NUMBER_TEST_RUNS; i>0; i--)
             {
-                value = (ushort)rnd.Next(0,65535);
+                value = (ushort)rnd.Next(0x00, 0xffff);
                 cpu.PC = value;
                 Assert.AreEqual(value, cpu.PC);
             }
